a84a77fbaebd5af9a03af9eea175c53187c897d6
AliA1997-Medium <devmtnali@gmail.com>
2018-08-20 19:02:46 -0700

    Finished workspaces controller

--- server/controllers/channels_controller.js
+++ server/controllers/channels_controller.js
@@ -1,11 +1,17 @@
 module.exports = {
     readChannels: (req, res) => {
-
+        const db = req.app.get('db');
+        db.read_channels().then(channels => {
+            res.json({channels}).end();
+        })
     },
     createChannel: (req, res) => {
-        
-    },
-    readMessages: (req, res) => {
-        
+        const db = req.app.get('db');
+        const { name, teamId, public } = req.body;
+        const newChannel = { name, teamId, public };
+        db.create_channel(newChannel).then(channels => {
+            ///Send the channel your creatd and the message, and end response. 
+            res.json({channel: channels[0], message: 'Channel Created!'}).end();
+        }).catch(err => console.log('Create a channel Error-------------', err));
     }
 }
